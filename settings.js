ac1983f144f1b3d3bffe2b25931bbbb25d08e90f
alialghozi <155251960+alialghozi@users.noreply.github.com>
2025-05-03 02:51:15 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'YEMEN TOB',
   author: '‎',
-  botName: "YEMEN TOP BOT ",
+  botName: "YEMEN TOP BOT",
   botOwner: 'ALI ALGHOZI', // Your name
   ownerNumber: '+60196176860', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
