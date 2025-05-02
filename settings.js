b98b09ff46b1cef5b6e40ceca69e21d37c64c4f2
alialghozi <155251960+alialghozi@users.noreply.github.com>
2025-05-03 03:38:07 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "YEMEN TOP BOT",
   botOwner: 'ALI ALGHOZI', // Your name
-  ownerNumber: '+60196176860', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '60196176860', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
